2e3949a5852b2c2bf52964c1fac15968d0a31d4d
ali sdg <alisdg1376@gmail.com>
2020-05-14 18:05:54 +0430

    v2

---
+++ front_end/nuxt.config.js
@@ -0,0 +1,82 @@
+const webpack = require("webpack");
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    script: [
+      { src: '/jquery.min.js' },
+      { src: '/bootstrap.min.js' },
+      { src: '/popper.min.js' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/bootstrap.min.css' },
+      { rel: 'stylesheet', href: '/Styles/Glob.css' },
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'ant-design-vue/dist/antd.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    //'@/plugins/antd-ui' ,
+    {src: '~plugins/datePicker.js', ssr: false},
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    babel: {
+      plugins: [
+        [
+          "import",
+          { libraryName: 'ant-design-vue', style: 'css' }
+        ]
+      ]
+    },
+    /*
+    ** Run ESLint on save
+    */
+    vendor: ["jquery", "bootstrap"],
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      }),
+      //new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ],
+    extend(config) {
+
+    },
+
+  }
+}
---
+++ front_end/pages/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <br>
+    <a-form layout="inline" :form="form" @submit="handleSubmit">
+      <a-form-item>
+        <a-input
+          v-decorator="[
+          'userName',
+          { rules: [{ required: true, message: 'لطفا نام کاربری خود را وارد کنید ' }] },
+        ]"
+          placeholder="نام کاربری"
+        >
+          <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item >
+        <a-input
+          v-decorator="[
+          'password',
+          { rules: [{ required: true, message: 'لطفا پسورد خود را وارد کنید ' }] },
+        ]"
+          type="password"
+          placeholder="پسورد"
+        >
+          <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit" >
+          وارد شوید
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Button from 'ant-design-vue/lib/button'
+import Form from 'ant-design-vue/lib/form'
+import Input from 'ant-design-vue/lib/input'
+import Icon from 'ant-design-vue/lib/icon'
+
+export default {
+  components: {
+    Logo , AButton: Button , AForm: Form , AFormItem: Form.Item , AInput: Input , AIcon: Icon
+  },
+    data(){
+      return{
+          form: this.$form.createForm(this, { name: 'login' }),
+      }
+    },
+    mounted() {
+      $(document).ready(function () {
+         console.log("Bia Karim inam az Jquery! :) \nvali ba axios request mizanimaaa! hanooz addesh nakrdm ==> w8 yekam :)")
+      })
+    },
+    methods:{
+        handleSubmit(e) {
+            e.preventDefault();
+            this.form.validateFields((err, values) => {
+                if (!err) {
+                    console.log('Karim Khan! Make Request to server pls ' , values);
+                }
+            });
+        },
+    }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+
+</style>
